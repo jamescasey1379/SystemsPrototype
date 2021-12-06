eec9918bec01b74f4eecfd118921ae650ce89ba1
John Foster <John.foster@ou.edu>
2021-12-06 15:16:09 -0600

    commit

--- SystemsPrototype/MainWindow.xaml.cs
+++ SystemsPrototype/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace SystemsPrototype
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Win1 OP = new Win1();
+            var host = new Window();
+            host.Content = OP;
+            host.Show();
+        }
     }
 }
